b1c2cda02f32cd6c7362eef379be28cd5cdd8553
reyyymartt <reymartcenteno03@gmail.com>
2024-11-13 10:44:11 +0800

    update

--- pages/admin.js
+++ pages/admin.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import * as Data from '/data/data_manager';
 import {navigate, showNotif} from '/app'
 
-const password = "MDYyMDIzcmV5"
-const pass = "110000 110110 110010 110000 110010 110011 1110010 1100101 1111001"
 const admin_ips = '124.83.104.174'
 
 class Pass extends React.Component{
@@ -12,7 +10,8 @@ class Pass extends React.Component{
 		super(props)
 		this.state = {
 			passInput: '',
-			ipa:''
+			ipa:'',
+			password:''
 		}
 	}
 	
@@ -20,9 +19,8 @@ class Pass extends React.Component{
 		this.setState({passInput: event.target.value})
 	}
 	buttonClicked = (event) => {
-		const convert1 = Data.convert(this.state.passInput)
-		const convert2 = btoa(this.state.passInput)
-		if (convert1==pass&&convert2==password){
+		
+		if (this.state.passInput==this.state.password){
 			//Data.sendMessage('Someone entered admin panel')
 			showNotif('System','Welcome admin!')
 			navigate('dashboard')
@@ -32,14 +30,11 @@ class Pass extends React.Component{
 	}
 	
 	componentDidMount() {
-		fetch("https://api.ipify.org?format=json")
-     .then(response => response.json())
-     .then(data => {
-        this.setState({ipa:data.ip})
-     })
-        .catch(error => {
-        console.error("Error fetching IP address:", error);
-     });
+		Data.readCollection("private").then((data)=>{
+			this.setState({password: data[0].data.password})
+		}).catch((error)=>{
+			console.error(error)
+		})
 	}
 	
 	
