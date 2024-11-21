0edfeca9bdaf86af9fb6c79e94e32919185786f2
reyyymartt <reymartcenteno03@gmail.com>
2024-11-21 22:06:06 +0800

    update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 SystemJS.config({
-  baseURL:'https://unpkg.com/',
+  baseURL: 'https://unpkg.com/',
   defaultExtension: true,
   packages: {
-    ".": {
+    '.': {
       main: './app.js',
       defaultExtension: 'js'
     }
@@ -17,11 +17,11 @@ SystemJS.config({
   map: {
     'plugin-babel': 'systemjs-plugin-babel@latest/plugin-babel.js',
     'systemjs-babel-build': 'systemjs-plugin-babel@latest/systemjs-babel-browser.js',
-    'react': 'react@17.0.1/umd/react.development.js',
-    'react-dom': 'react-dom@17.0.1/umd/react-dom.development.js'
+    'react': 'https://unpkg.com/react@17.0.2/umd/react.production.min.js',
+    'react-dom': 'https://unpkg.com/react-dom@17.0.2/umd/react-dom.production.min.js',
+    'react-router-dom': 'https://unpkg.com/react-router-dom@6/umd/react-router-dom.production.min.js'
   },
   transpiler: 'plugin-babel'
 });
 
-SystemJS.import('./app')
-  .catch(console.error.bind(console));
+SystemJS.import('./app').catch(console.error.bind(console));
